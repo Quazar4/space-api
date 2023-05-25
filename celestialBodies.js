225019910e816110f4516e13dd5e6bdc8106488e
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-05-25 09:08:40 +0530

    Create celestialBodies.js

---
+++ celestialBodies.js
@@ -0,0 +1,49 @@
+// The image links are sourced from the internet
+
+const celestialBodies = [
+    {
+      name: 'Mercury',
+      image: 'https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/AW_Mercury_ladprw.jpg',
+      description: 'Mercury is the first planet from the sun'
+    },
+    {
+      name: 'Venus',
+      image: 'https://www.astronomy.com/wp-content/uploads/sites/2/2023/05/ASY-VN0523_02.jpg?fit=620%2C349',
+      description: 'Venus is the second planet from the sun and is the hottest planet in the solar system'
+    },
+    {
+      name: 'Earth',
+      image: 'https://cdn.mos.cms.futurecdn.net/yCPyoZDQBBcXikqxkeW2jJ-1200-80.jpg',
+      description: 'The third planet from the sun'
+    },
+    {
+      name: 'Mars',
+      image: 'https://starwalk.space/gallery/images/mars-the-ultimate-guide/1920x1080.jpg',
+      description: 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System. It is a terrestrial planet with a thin atmosphere, and it is home to the largest volcano and the deepest, longest canyon in the Solar System.'
+    },
+    {
+      name: 'Jupiter',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/c/c1/Jupiter_New_Horizons.jpg',
+      description: 'Jupiter is the largest planet in the Solar System. It is a gas giant with a prominent system of stripes and a great red spot, which is a storm larger than Earth that has been ongoing for at least 150 years.'
+    },
+    {
+      name: 'Saturn',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/c/c7/Saturn_during_Equinox.jpg',
+      description: 'Saturn is the 6th planet from the sun'
+    },
+    {
+      name: 'Uranus',
+      image: 'https://ychef.files.bbci.co.uk/1280x720/p0257vk5.jpg',
+      description: 'Saturn is the 7th planet from the sun'
+    },
+    {
+      name: 'Neptune',
+      image: 'https://cdn.theatlantic.com/thumbor/t1XshjajMlYAJsA29dP7Swq5h-E=/4x51:2491x1357/960x504/media/img/mt/2022/06/PIA01492_Edit/original.jpg',
+      description: 'Neptune is the 8th planet from the sun'
+    },
+    {
+      name: 'Sagittarius A',
+      image: 'https://images.immediate.co.uk/production/volatile/sites/4/2022/05/Sgr-A-EHT-image-hero-e962528.jpg?quality=90&resize=940,400',
+      description: 'The black hole in the center of the milky way'
+    }
+  ];
